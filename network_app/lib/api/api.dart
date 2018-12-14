3e1525e8e47f218a3809c6cba858fde64803843a
ReverseScale <ReverseScale@iCloud.com>
2018-12-14 11:06:16 +0800

    ✨:Introducing new features for Network.

---
+++ network_app/lib/api/api.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+class Api{
+
+  void fetchUrl(String url, Function success, Function error, Function empty) async {
+    Dio dio = Dio();
+    Response response = await dio.get(url);
+    int code = response.statusCode;
+    if (code >= 200 && code <= 300) {
+      Map<String, dynamic> data = response.data;
+      if (data != null) {
+        success(data);
+      } else {
+        empty();
+      }
+    } else {
+      error('请求失败: $code');
+    }
+  }
+}
---
+++ network_app/lib/provider_block.dart
@@ -0,0 +1,67 @@
+import 'package:network_app/api/const.dart';
+import 'package:network_app/model/search_model.dart';
+import 'package:network_app/api/api.dart';
+
+import 'package:flutter/widgets.dart';
+import 'package:rxdart/rxdart.dart';
+
+class ProviderBlock {
+  final String _EMPTY = "_empty_";
+
+  final _api = new Api();
+  final _fetcher = new PublishSubject<SearchModel>();
+  stream() => _fetcher.stream;
+
+  void dispose() {
+    if (!_fetcher.isClosed) {
+      _fetcher.close();
+    }
+  }
+
+  void fetchQueryList() async {
+    fetchUrl(SEARCH_URL, (map) => SearchModel.fromJson(map));
+  }
+
+  void fetchUrl(String url, Function handleData) async {
+    _api.fetchUrl(url, (map) {
+      _fetcher.sink.add(handleData(map));
+    }, (errorMsg) {
+      _fetcher.sink.addError(errorMsg, null);
+    }, () {
+      _fetcher.sink.addError(_EMPTY);
+    });
+  }
+
+  Widget streamBuilder<T>({
+    T initalData,
+    Function success,
+    Function error,
+    Function empty,
+    Function loading,
+    Function finished,
+  }) {
+    return StreamBuilder(
+        stream: stream(),
+        initialData: initalData,
+        builder: (context, AsyncSnapshot<T> snapshot) {
+          if (finished != null) {
+            finished();
+          }
+          if (snapshot.hasData) {
+            if (success != null) return success(snapshot.data);
+          } else if (snapshot.hasError) {
+            final errorStr = snapshot.error;
+            if (errorStr == _EMPTY) {
+              if (empty != null) return empty();
+            } else {
+              if (error != null) return error(errorStr);
+            }
+          } else {
+            if (loading != null) return loading();
+          }
+        }
+      );
+  }
+  
+  static ProviderBlock newInstance() => new ProviderBlock();
+}

ea70c253725d65615b4e33a130bcf02da861816c
ReverseScale <ReverseScale@iCloud.com>
2018-12-28 13:14:09 +0800

    ♻️:Refactoring code.

--- hello_flutter_app/lib/BasisApp.dart
+++ hello_flutter_app/lib/BasisApp.dart
@@ -423,90 +423,77 @@ class TextStylePage extends StatefulWidget {
 class TextStyleState extends State<TextStylePage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
+    return Scaffold(
+        appBar: AppBar(
           // 去掉导航栏下面的阴影
           elevation: 0.0,
-          title: new Text('TextStyle'),
+          title: Text('TextStyle'),
         ),
-        body: new Container(
+        body: Container(
           margin: EdgeInsets.all(10.0),
-          child: new Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              new Text(
+              Text(
                 'inherit: 为 false 的时候不显示',
-                style: new TextStyle(
+                style: TextStyle(
                   inherit: true,
                 ),
               ),
-              new Text(
+              Text(
                 'color/fontSize: 字体颜色，字号等',
-                style: new TextStyle(
+                style: TextStyle(
                   color: Color.fromARGB(255, 150, 150, 150),
                   fontSize: 22.0,
                 ),
               ),
-              new Text(
+              Text(
                 'fontWeight: 字重',
-                style: new TextStyle(fontWeight: FontWeight.w700),
+                style: TextStyle(fontWeight: FontWeight.w700),
               ),
-              new Text(
+              Text(
                 'fontStyle: FontStyle.italic 斜体',
-                style: new TextStyle(
+                style: TextStyle(
                   fontStyle: FontStyle.italic,
                 ),
               ),
-              new Text(
+              Text(
                 'letterSpacing: 字符间距',
-                style: new TextStyle(
+                style: TextStyle(
                   letterSpacing: 10.0,
                   // wordSpacing: 15.0
                 ),
               ),
-              new Text(
+              Text(
                 'wordSpacing: 字或单词间距',
-                style: new TextStyle(
+                style: TextStyle(
                     // letterSpacing: 10.0,
                     wordSpacing: 15.0),
               ),
-              new Text(
+              Text(
                 'textBaseline:这一行的值为TextBaseline.alphabetic',
-                style: new TextStyle(
-                    textBaseline: TextBaseline.alphabetic),
+                style: TextStyle(textBaseline: TextBaseline.alphabetic),
               ),
-              new Text(
+              Text(
                 'textBaseline:这一行的值为TextBaseline.ideographic',
-                style: new TextStyle(
-                    textBaseline: TextBaseline.ideographic),
+                style: TextStyle(textBaseline: TextBaseline.ideographic),
               ),
-              new Text(
-                'height: 用在Text控件上的时候，会乘以fontSize做为行高,所以这个值不能设置过大',
-                style: new TextStyle(
-                  height: 1.0,
-                )
-              ),
-              new Text(
-                'decoration: TextDecoration.overline 上划线',
-                style: new TextStyle(
-                  decoration: TextDecoration.overline,
-                  decorationStyle: TextDecorationStyle.wavy
-                )
-              ),
-              new Text(
-                'decoration: TextDecoration.lineThrough 删除线',
-                style: new TextStyle(
-                  decoration: TextDecoration.lineThrough,
-                  decorationStyle: TextDecorationStyle.dashed
-                )
-              ),
-              new Text(
-                'decoration: TextDecoration.underline 下划线',
-                style: new TextStyle(
-                  decoration: TextDecoration.underline,
-                  decorationStyle: TextDecorationStyle.dotted
-                )
-              ),
+              Text('height: 用在Text控件上的时候，会乘以fontSize做为行高,所以这个值不能设置过大',
+                  style: TextStyle(
+                    height: 1.0,
+                  )),
+              Text('decoration: TextDecoration.overline 上划线',
+                  style: TextStyle(
+                      decoration: TextDecoration.overline,
+                      decorationStyle: TextDecorationStyle.wavy)),
+              Text('decoration: TextDecoration.lineThrough 删除线',
+                  style: TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                      decorationStyle: TextDecorationStyle.dashed)),
+              Text('decoration: TextDecoration.underline 下划线',
+                  style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.dotted)),
             ],
           ),
         ));

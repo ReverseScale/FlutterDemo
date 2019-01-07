fbfc3c353e7b80c879d011a80fd1033f7cdb0b41
ReverseScale <ReverseScale@iCloud.com>
2019-01-07 10:44:33 +0800

    ✨:Introducing new features for Transition.

--- hello_flutter_app/lib/RouterAnimationApp/custom_router.dart
+++ hello_flutter_app/lib/RouterAnimationApp/custom_router.dart
@@ -15,14 +15,38 @@ class CustomRouter extends PageRouteBuilder {
             transitionsBuilder: (BuildContext context,
                 Animation<double> animation1,
                 Animation<double> animation2,
-                Widget child
-                ) {
-                  // 渐变 路由动画
-                  return FadeTransition(
-                    opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: animation1, curve: Curves.fastOutSlowIn)),
-                  child: child,
-                  );
+                Widget child) {
+              // 渐变 路由动画
+              // return FadeTransition(
+              //   opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: animation1, curve: Curves.fastOutSlowIn)),
+              // child: child,
+              // );
 
-                  // 缩放 路由动画
-                });
+              // 缩放 路由动画
+              // return ScaleTransition(
+              //   scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: animation1,curve: Curves.fastOutSlowIn)),
+              //   child: child,
+              // );
+
+              // 旋转-缩放 路由动画
+              // return RotationTransition(
+              //   turns: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //       parent: animation1, curve: Curves.fastOutSlowIn)),
+              //   child: ScaleTransition(
+              //     scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+              //         parent: animation1, curve: Curves.fastOutSlowIn)),
+              //     child: child,
+              //   ),
+              // );
+
+              // 滑动 路由动画
+              return SlideTransition(
+                position: Tween<Offset>(
+                        begin: Offset(-1.0, 0.0), end: Offset(0.0, 0.0))
+                    .animate(CurvedAnimation(
+                        parent: animation1, curve: Curves.fastOutSlowIn)),
+                child: child,
+              );
+              
+            });
 }

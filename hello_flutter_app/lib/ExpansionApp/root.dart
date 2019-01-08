023b454b64fcd8616176dcddabdca25717d48012
ReverseScale <ReverseScale@iCloud.com>
2019-01-08 09:59:45 +0800

    ✨:Introducing new features for ExpansionList.

--- hello_flutter_app/lib/ExpansionApp/root.dart
+++ hello_flutter_app/lib/ExpansionApp/root.dart
@@ -6,11 +6,78 @@ class ExpansionApp extends StatelessWidget {
     return MaterialApp(
       title: 'Closed',
       theme: ThemeData.dark(),
-      home: ExpansionHome(),
+      home: ExpansionListHome(),
     );
   }
 }
 
+class ExpansionStateBean {
+  var isOpen;
+  var index;
+  ExpansionStateBean(this.index, this.isOpen);
+}
+
+// 列表 Expansion
+class ExpansionListHome extends StatefulWidget {
+  @override
+  _ExpansionListHomeState createState() => _ExpansionListHomeState();
+}
+
+class _ExpansionListHomeState extends State<ExpansionListHome> {
+  List<int> mList;
+  List<ExpansionStateBean> expandStateList;
+
+  _ExpansionListHomeState() {
+    mList = new List();
+    expandStateList = new List();
+
+    for (int i = 0; i < 10; i++) {
+      mList.add(i);
+      expandStateList.add(ExpansionStateBean(i, false));
+    }
+  }
+
+  _setCurrentIndex(int index, isExpand) {
+    setState(() {
+      expandStateList.forEach((item) {
+        if (item.index == index) {
+          item.isOpen = !isExpand;
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expansion List'),
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: ExpansionPanelList(
+          expansionCallback: (index, bol) {
+            _setCurrentIndex(index,bol);
+          },
+          children: mList.map((index) {
+            return ExpansionPanel(
+                headerBuilder: (context, isExpanded) {
+                  return ListTile(
+                    title: Text('This is No.$index'),
+                  );
+                },
+                body: ListTile(
+                  title: Text('你看到我拉🌶🌶🌶$index'),
+                ),
+                isExpanded: expandStateList[index].isOpen);
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+// 单条 Expansion
 class ExpansionHome extends StatefulWidget {
   @override
   _ExpansionHomeState createState() => _ExpansionHomeState();

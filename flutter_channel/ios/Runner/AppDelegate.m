87eec9ec5455cf488952ab850255727a80dc9931
Tim <reversescale@icloud.com>
2019-06-28 16:02:22 +0800

    ✨:Introducing new features for native channel.

---
+++ flutter_channel/ios/Runner/AppDelegate.m
@@ -0,0 +1,48 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+
+#import <Flutter/Flutter.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+    
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    
+    FlutterMethodChannel* batteryChannel = [FlutterMethodChannel methodChannelWithName:@"samples.flutter.io/battery" binaryMessenger:controller];
+    
+    [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        if ([@"getBatteryLevel" isEqualToString:call.method]) {
+            int batteryLevel = [self getBatteryLevel];
+            
+            if (batteryLevel == -1) {
+                result([FlutterError errorWithCode:@"UNAVAILABLE"
+                                           message:@"Battery info unavailable"
+                                           details:nil]);
+            } else {
+                result(@(batteryLevel));
+            }
+        } else if ([@"getMessage" isEqualToString:call.method]) {
+            result(@"Message is Hi iOS from CHANNEL");
+        } else {
+            result(FlutterMethodNotImplemented);
+        }
+    }];
+  
+    
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (int)getBatteryLevel {
+    UIDevice* device = UIDevice.currentDevice;
+    device.batteryMonitoringEnabled = YES;
+    if (device.batteryState == UIDeviceBatteryStateUnknown) {
+        return -1;
+    } else {
+        return (int)(device.batteryLevel * 100);
+    }
+}
+
+@end
